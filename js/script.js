06aa9cd67a4568e2fe04ba6a0aca3d9ddabc7e44
James Kofi Myers <myers_james59@yahoo.com>
2023-10-09 21:40:56 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let typed = new Typed('.words', {
+    strings: ['Frontend Developer!', 'MERN Stack Developer!'],
+    typedSpeed: 50,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+
+})
