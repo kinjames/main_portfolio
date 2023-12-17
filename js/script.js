4bf6071fa0aab7c36b7356f45143af941ea44f8b
James Kofi Myers <myers_james59@yahoo.com>
2023-12-17 19:16:45 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-let typed = new Typed('.words', {
-    strings: ['Frontend Developer!', 'MERN Stack Developer!'],
-    typedSpeed: 50,
-    backSpeed: 100,
-    backDelay: 2000,
-    loop: true,
+let typed = new Typed(".words", {
+  strings: ["Frontend Developer!", "MERN Stack Developer!"],
+  typedSpeed: 50,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
 
-})
+let btns = document.querySelectorAll(".nav-link");
