5249f37e42e6ba319c8f792810106cd5546034bc
James Kofi Myers <myers_james59@yahoo.com>
2023-12-19 11:13:23 +0000

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,44 @@ let typed = new Typed(".words", {
   backDelay: 2000,
   loop: true,
 });
+
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
+
+function toggleActiveState() {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 0;
+
+    if (pageYOffset >= sectionTop) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+}
+
+function handleClick(event) {
+  // event.preventDefault();
+  const targetId = event.target.getAttribute("href").substring(1);
+  const targetSection = document.getElementById(targetId);
+
+  if (targetSection) {
+    window.scrollTo({
+      top: targetSection.offsetTop - 50,
+      behavior: "smooth",
+    });
+  }
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", handleClick);
+});
+
+window.addEventListener("scroll", toggleActiveState);
