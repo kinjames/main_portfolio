796e1bc3111dc231688d1fcc6f32d8b1d2ff2aea
James Kofi Myers <myers_james59@yahoo.com>
2023-12-19 08:44:30 +0000

    update

--- js/script.js
+++ js/script.js
@@ -5,22 +5,3 @@ let typed = new Typed(".words", {
   backDelay: 2000,
   loop: true,
 });
-
-const navLinks = document.querySelectorAll(".nav-link");
-function handleClick(event) {
-  // Prevent the default behavior of anchor links
-  event.preventDefault();
-
-  // Remove active class from all links
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-  });
-
-  // Add active class to the clicked link
-  event.target.classList.add("active");
-}
-
-// Attach click event listeners to all navigation links
-navLinks.forEach((link) => {
-  link.addEventListener("click", handleClick);
-});
